dcc923774cea8159158f8a203fbb2bf3a3153b50
Thotajyothsna <thotajyothsna2001@gmail.com>
2024-05-27 15:30:59 +0530

    Retrieving all the records/data present in Employee_payroll Table

---
+++ Retrieving_All_Data.sql
@@ -0,0 +1,8 @@
+--Retrieving all columns of data and also all records of table(employee_payroll)
+SELECT * 
+FROM employee_payroll;
+
+--retrieving all records of particular columns that we want from table(employee_payroll)
+SELECT SALARY , NAME  
+FROM employee_payroll;
+
