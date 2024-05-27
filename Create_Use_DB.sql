bf1211df59119657ca182b4c903537de0eed13af
Thotajyothsna <thotajyothsna2001@gmail.com>
2024-05-27 15:08:15 +0530

    Creating database payroll_service and using

--- Create_Use_DB.sql
+++
@@ -1,9 +0,0 @@
-
---creating database named payroll_service
-CREATE DATABASE payroll_service;
-
---To get the list of databases
-EXEC sp_databases;
-
---using the database payroll
-USE payroll_service;
